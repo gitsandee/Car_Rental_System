737314bb90cb120ddacbfdffb9fc1280ac392c93
SAHANYA99 <84034000+SAHANYA99@users.noreply.github.com>
2022-05-07 21:25:22 +0530

    add backend files

---
+++ backend/server.js
@@ -0,0 +1,102 @@
+const express = require("express");
+cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.sendFile("D:/PROJECTS/vehicleRent/backend" + "/index.html");
+});
+
+var db;
+
+MongoClient.connect(
+  "mongodb://127.0.0.1:27017/vehicleRent",
+  (err, database) => {
+    if (err) return console.log(err);
+    db = database.db("carCastle");
+
+
+    //Form types crud
+
+    app.post("/formTypes/add", (req, res) => {
+      db.collection("formTypes").insertOne(req.body, (err, result) => {
+        if (err) return res.send(400, { message: err });
+        res.send(200, { message: "ok" });
+      });
+    });
+
+    app.post("/formTypes/update", (req, res) => {
+      db.collection("formTypes").updateOne(req.body.query, req.body.newVals, (err, result) => {
+        if (err) return res.send(400, { message: err });
+        res.send(200, { message: "ok" });
+      });
+    });
+
+    app.get("/formTypes", (req, res) => {
+      db.collection("formTypes")
+        .find({})
+        .toArray(function (err, result) {
+          if (err) throw err;
+          res.send(200, { message: "ok", data: result });
+        });
+    });
+
+    app.post("/formTypes/delete", (req, res) => {
+      db.collection("formTypes").deleteOne(req.body.id, function (err, obj) {
+        if (err) throw err;
+        console.log("1 document deleted");
+        res.send(200, { message: "ok" });
+      });
+    });
+
+
+    //////////////////////////
+
+    //Form submissions crud
+
+    app.post("/formSubmissions/add", (req, res) => {
+      db.collection("formSubmissions").insertOne(req.body, (err, result) => {
+        if (err) return res.send(400, { message: err });
+        res.send(200, { message: "ok" });
+      });
+    });
+
+    app.post("/formSubmissions/update", (req, res) => {
+      console.log(req.body.query, req.body.newVals)
+      db.collection("formSubmissions").updateOne(req.body.query, { $set: { formDetails: req.body.newVals } }, (err, result) => {
+        if (err) return res.send(400, { message: err });
+        res.send(200, { message: "ok" });
+      });
+    });
+
+    app.get("/formSubmissions", (req, res) => {
+      console.log({ req: req.body, query: req.query })
+      db.collection("formSubmissions")
+        .find(req.query ? { _id: req.query.id } : {})
+        .toArray(function (err, result) {
+          if (err) throw err;
+          res.send(200, { message: "ok", data: result });
+        });
+    });
+
+    app.post("/formSubmissions/delete", (req, res) => {
+      db.collection("formSubmissions").deleteOne(req.body.id, function (err, obj) {
+        if (err) throw err;
+        console.log("1 document deleted");
+        res.send(200, { message: "ok" });
+      });
+    });
+    //////////////////////////
+
+
+
+    app.listen(8000, () => {
+      console.log("listening on 8000");
+    });
+  }
+);
